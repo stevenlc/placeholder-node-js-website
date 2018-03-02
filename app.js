6676e7df8dc94abae0f406c7e2c771d5f1d20e36
Steven Camina <stevencamina@Stevens-MacBook-Pro.local>
2018-03-02 12:16:06 -0500

    version 2

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", function(req, res) {
-    res.render("home.ejs");
+    res.render("home");
 });
 
-app.listen("5000", process.env.IP, function() {
-    console.log("SERVER for placeholder HAS STARTED.");
+app.listen("7000", process.env.IP, function() {
+    console.log("Server for placeholder nodeJS website started.");
 });
